8895417169692173bd7b04732e023dca9eadd3cd
今夕歸鄉 <sugky7302@gmail.com>
2020-02-05 22:56:24 +0800

    todo

--- src/lib/effect_manager.lua
+++ src/lib/effect_manager.lua
@@ -99,4 +99,10 @@ function EffectManager:getTemplate(name)
     return self._templates_[name]
 end
 
+function EffectManager:pause(target, name)
+end
+
+function EffectManager:resume(target, name)
+end
+
 return EffectManager
