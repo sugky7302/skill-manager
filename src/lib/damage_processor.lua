e68af18af489a66c80536b3d3005cd846b7e98e6
Arvin <sugky7302@gmail.com>
2020-02-25 13:40:06 +0800

    缺計算攻擊力和防禦力

--- src/lib/damage_processor.lua
+++ src/lib/damage_processor.lua
@@ -1,14 +1,15 @@
 local require = require
 local skill_manager = require 'lib.skill_manager':new()
 local Math = require 'std.math'
-local DP = require 'std.class'('DamageProcessor')
-local AddUp, Parse, Judge, Calculate, Process, IsDodge, IsPenetrated, CheckCrit, GetAttack, GetDefense, DealDamage
 
-local STATUS = {'穿透', '正常', '閃避', '格擋'}
-local TypeA = {}
-local TypeB = {}
-local TypeC = {}
-local TypeD = {}
+local DP = require 'std.class'('DamageProcessor')
+local AddUp, Parse, Judge, Calculate, Process, IsDodge, IsPenetrated
+local IsCrit, GetAttack, GetDefense, DealDamage, GetRatio
+
+local TYPE_A = {}
+local TYPE_B = {}
+local TYPE_C = {}
+local TYPE_D = {}
 
 function DP:_new()
     if not self._instance_ then
@@ -24,10 +25,11 @@ function DP.run(name, source, target)
         source = source,
         target = target,
         value = 0,  -- 傷害值
-        rate = nil, -- 物理傷害和法術傷害的占比。左邊是物理、右邊是法術
-        proc = 0, -- 法術攻擊力的倍率係數
-        is_crit = false, -- 是否暴擊。暴擊=true，正常=false
-        status = 2 -- 狀態判定。判定過程可能出現閃避、穿透等特殊現象，1=穿透、2=正常、3=閃避、4=格擋
+        rate = nil,  -- 物理傷害和法術傷害的占比。左邊是物理、右邊是法術
+        proc = 0,  -- 法術攻擊力的倍率係數
+        is_crit = 0,  -- 是否暴擊。暴擊=1，正常=0
+        is_blk = 1,  -- 是否穿透。格擋=1，正常=0
+        status = 0  -- 狀態判定。判定過程可能出現閃避、穿透等特殊現象，0=正常、1=穿透、2=閃避、4=格擋、8=暴擊
     }
 
     if not Parse(event) then
@@ -41,7 +43,7 @@ function DP.run(name, source, target)
     Calculate(event)
     Process(event)
 
-    return STATUS[event.status], event.value
+    return event.status, event.value
 end
 
 Parse = function(event)
@@ -66,40 +68,62 @@ end
 
 Judge = function(event)
     if IsDodge(event.source, event.target) then
-        event.status = 3
+        event.status = 2
         return false
     end
 
     if IsPenetrated(event.source, event.target) then
+        event.is_blk = 0
         event.status = 1
-    else
-        event.status = 4
+    end
+
+    if IsCrit(event.source, event.target) then
+        event.is_crit = 1
+        event.status = event.status + 8
     end
 
     return true
 end
 
 IsDodge = function(source, target)
+    local p = Math.rand(0, 100)
+    local hit = GetRatio(source:getAttribute('命中'), source:getAttribute("等級"), 60, 50)
+    local dodge = GetRatio(target:getAttribute('閃避'), target:getAttribute("等級"), 60, 50)
+
+    if dodge > hit then
+        return p < 80 + 100 * (dodge - hit)
+    elseif dodge == hit then
+        return p < 51
+    else
+        return p >= 80 + 100 * (hit - dodge)
+    end
 end
 
 IsPenetrated = function(source, target)
+    local pnt = GetRatio(source:getAttribute('穿透'), source:getAttribute("等級"), 60, 50)
+    local blk = GetRatio(target:getAttribute('格擋'), target:getAttribute("等級"), 60, 50)
+
+    return pnt > blk
+end
+
+IsCrit = function(source, target)
+    local crt = GetRatio(source:getAttribute('暴擊'), source:getAttribute("等級"), 60, 50)
+    local act = GetRatio(target:getAttribute('韌性'), target:getAttribute("等級"), 60, 50)
+    return crt > act
+end
+
+GetRatio = function(x, y, k, b)
+    return x / (x + k * y + b)
 end
 
 Calculate = function(event)
-    event.is_crit = CheckCrit(event.source, event.target)
-
-    -- NOTE: 這裡status-1是因為status=1時為穿透，status-1=0，剛好能夠消除def
     local atk, def = GetAttack(event.source, event.rate, event.proc), GetDefense(event.target, event.rate)
     event.value =
         Math.bound(
         0.05 * atk,
-        (event.is_crit + 1) * (atk - (event.status - 1) * def),
+        (event.is_crit + 1) * (atk - event.is_blk * def),
         0.95 * event.target:getAttribute('生命上限')
-    ) - event.target:getAttribute("護盾")
-end
-
-CheckCrit = function(source, target)
-    return 1 or 0
+    ) - event.target:getAttribute('護盾')
 end
 
 GetAttack = function(source, rate, proc)
@@ -125,15 +149,15 @@ Process = function(event)
     -- NOTE: 不用add的原因是value = 傷害 - 護盾 => -value = 護盾 - 傷害 = 剩餘護盾量
     --       每次都要計算護盾量。因為如果value > 0，表示破盾，max(0, -value) = 0，剛好將護盾值設為0；
     --       如果value < 0，表示還有護盾，max(0, -value) = 剩餘護盾量，也剛好將護盾值設為剩餘護盾量。
-    event.target:setAttribute("護盾", Math.max(0, -event.value))
+    event.target:setAttribute('護盾', Math.max(0, -event.value))
 
     -- 儲存最後傷害，有些技能會用到
-    event.target:setAttribute("最後造成的傷害", event.value)
+    event.target:setAttribute('最後造成的傷害', event.value)
 end
 
 DealDamage = function(target, value)
-    if value < target:getAttribute("生命") then
-        target:addAttribute("生命", -value)
+    if value < target:getAttribute('生命') then
+        target:addAttribute('生命', -value)
     else
         require 'war3.enhanced_jass'.removeUnit(target:getObject())
     end
