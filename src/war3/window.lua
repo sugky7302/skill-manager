5dd0afe0715a43b17777438d374eb3c003a86e57
今夕歸鄉 <sugky7302@gmail.com>
2020-04-05 10:38:45 +0800

    完善window

--- src/war3/window.lua
+++ src/war3/window.lua
@@ -1,30 +1,51 @@
 local require = require
 local type = type
+local math = math
 local ej = require 'war3.enhanced_jass'
+local event_manager = require 'lib.event_manager':new()
+local Listener = require 'war3.listener'
 
 local Window = require 'std.class'('Window')
-local Update
+local Refresh, SetTitle, CreateItem, CreateOperator, FixPageNumber
+
+event_manager:addEvent(
+    '對話框-被點擊',
+    'GetClickedDialog GetClickedButton',
+    function(_, dialog, button)
+        local window = Window(dialog)
+
+        if window:isPrev(button) then
+            window:prev()
+        elseif window:isNext(button) then
+            window:next()
+        end
+    end
+)
 
 function Window:_new(player, max_length_for_one_page)
     local this = {
         _object_ = ej.DialogCreate(), -- NOTE: 不能在初始化時創建
         _user_ = player,
-        _lists_ = nil,
-        _items_ = nil,
+        _lists_ = {},
+        _items_ = {},
         _page_number_ = 1,
         _length_ = max_length_for_one_page or 6,
         _operator_ = {false, false, false}, -- 關閉鈕、下一頁、上一頁
         _is_opened_ = false,
+        _title_ = nil
     }
 
     -- 將實例綁在類別上，方便呼叫
-    self[player] = this
+    self[this._object_] = this
+
+    -- 對此物件綁定操作鈕的觸發
+    Listener:new(event_manager)('對話框-被點擊')(this._object_)
 
     return this
 end
 
-function Window:__call(player)
-    return Window[player] or self:new(player)
+function Window:__call(object)
+    return Window[object]
 end
 
 function Window:isPrev(item)
@@ -39,17 +60,6 @@ function Window:getObject()
     return self._object_
 end
 
-function Window:setTitle(title)
-    ej.DialogSetMessage(self._object_, title)
-    return self
-end
-
-function Window:open()
-    ej.DialogDisplay(self._user_, self._object_, true)
-    self._is_opened_ = true
-    return self
-end
-
 function Window:close()
     ej.DialogDisplay(self._user_, self._object_, false)
     self._is_opened_ = false
@@ -57,10 +67,6 @@ function Window:close()
 end
 
 function Window:findText(key)
-    if not self._lists_ then
-        return nil
-    end
-
     if type(key) == 'string' then
         return key
     end
@@ -68,48 +74,48 @@ function Window:findText(key)
     if key > #self._lists_ then
         return self._lists_[self._items_[key]]
     end
-    
+
     return self._lists_[key]
 end
 
-function Window:setLength(new_length)
-    self._length_ = new_length
-    Update(self)
+function Window:setTitle(title)
+    self._title_ = title
+    Refresh(self)
+    return self
+end
+
+function Window:open()
+    ej.DialogDisplay(self._user_, self._object_, true)
+    self._is_opened_ = true
+    Refresh(self)
     return self
 end
 
 function Window:next()
     self._page_number_ = self._page_number_ + 1
-    Update(self)
+    Refresh(self)
     return self
 end
 
 function Window:prev()
     self._page_number_ = self._page_number_ - 1
-    Update(self)
+    Refresh(self)
     return self
 end
 
 function Window:add(text)
-    if not self._lists_ then
-        self._lists_ = {}
-    end
-
-    self._lists_[#self._lists_+1] = text
-    Update(self)
+    self._lists_[#self._lists_ + 1] = text
+    Refresh(self)
 
     return self
 end
 
 function Window:delete(key)
-    if not self._lists_ then
-        return
-    end
-
     local number = self:findRank(key)
     if number then
         table.remove(self._lists_, number)
-        Update(self)
+        FixPageNumber(self)
+        Refresh(self)
     end
 
     return self
@@ -127,38 +133,73 @@ function Window:findRank(key)
     if key > #self._lists_ then
         return self._items_[key]
     end
-    
+
     return key
 end
 
-Update = function(self)
+function Window:setLength(new_length)
+    self._length_ = new_length
+    FixPageNumber(self)
+    Refresh(self)
+    return self
+end
+
+FixPageNumber = function(self)
+    -- NOTE: 修正後可能會產生該頁沒選項的情況，這時候必須將頁碼調到最後一頁
+    self._page_number_ = math.min(self._page_number_, math.ceil(#self._lists_ / self._length_))
+end
+
+Refresh = function(self)
     self:clear()
-
-    local item
-    for i = (self._page_number_-1) * self._length_ + 1, math.min(self._page_number_ * self._length_, #self._lists_) do
-        item = ej.DialogAddButton(self._object_, self._lists_[i], 0)
-        self._items_[item] = i
-    end
-
-    -- 添加操作鈕
-    if self._page_number_ > 1 then
-        self._operator_[3] = ej.DialogAddButton(self._object_, '|cff00ff00↑', 0)
-    end
-
-    if self._page_number_ * self._length_ < #self._lists_ then
-        self._operator_[2] = ej.DialogAddButton(self._object_, '|cff00ff00↓', 0)
-    end
-
-    self._operator_[1] = ej.DialogAddButton(self._object_, '|cffff0000x', 0)
+    SetTitle(self)
+    CreateItem(self)
+    CreateOperator(self)
 
     if self._is_opened_ then
         ej.DialogDisplay(self._user_, self._object_, true)
     end
 end
 
+SetTitle = function(self)
+    if self._title_ then
+        ej.DialogSetMessage(self._object_, self._title_)
+    end
+end
+
+CreateItem = function(self)
+    local item
+    for i = (self._page_number_ - 1) * self._length_ + 1, math.min(self._page_number_ * self._length_, #self._lists_), 1 do
+        item = ej.DialogAddButton(self._object_, self._lists_[i], 0)
+        self._items_[item] = i
+    end
+end
+
+-- NOTE: hotkey是ascii表查詢該按鍵於10進位的值
+CreateOperator = function(self)
+    local concat = table.concat
+
+    if self._page_number_ > 1 then
+        self._operator_[3] =
+            ej.DialogAddButton(self._object_, concat {'|cff00ff00↑|r(p.', self._page_number_ - 1, ')'}, 0)
+    end
+
+    if self._page_number_ * self._length_ < #self._lists_ then
+        self._operator_[2] =
+            ej.DialogAddButton(self._object_, concat {'|cff00ff00↓|r(p.', self._page_number_ + 1, ')'}, 0)
+    end
+
+    self._operator_[1] = ej.DialogAddButton(self._object_, '|cffff0000x|r', 0)
+end
+
 function Window:_remove()
-    self:clear()
+    self:reset()
     ej.DialogDestroy(self._object_)
+end
+
+function Window:reset()
+    self:clear()
+    self._lists_ = {}
+    return self
 end
 
 function Window:clear()
